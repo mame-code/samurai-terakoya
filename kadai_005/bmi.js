c29e92c48c1c9c61294c4eb43a1f7841252ae94c
mame-code <149633750+mame-code@users.noreply.github.com>
2023-11-08 22:04:33 +0900

    課題005提出

---
+++ kadai_005/bmi.js
@@ -0,0 +1,22 @@
+//体重
+let userWeight;
+
+//値の代入
+userWeight='68';
+
+//身長
+let userHeight;
+
+//値の代入
+userHeight='1.7';
+
+//BMI
+let userBmi;
+
+//値の代入
+userBmi=userWeight/(userHeight*userHeight);
+
+
+
+//コンソールへの出力
+console.log(userBmi);
